15d9b3d3b008721091202417f8860f89aa723ea4
SmexyShimala <jshimala@saintjoe.edu>
2015-03-02 16:00:40 -0600

    Update HouseDriver.java

--- HouseDriver.java
+++ HouseDriver.java
@@ -1,1 +1,16 @@
+public class HouseDriver {
+  public static void main(String[] args) {
+	
+	House Beach = new House();
+  Beach.name = "Beach House'';
+	Beach.place = beach;
+	Beach.description = "It's the beach house";
+	System.out.println(beach.toString());
 
+  House Home = new House();
+  home.name = home;
+  home.place = rensaltucky;
+  home.description = "It's Home man";
+  System.out.println(home.toString());
+	}
+}
