c0cc7acb0ffa7d7136f95037832d9723c2fee0a8
SmexyShimala <jshimala@saintjoe.edu>
2015-03-02 15:54:58 -0600

    Update House.java

--- House.java
+++ House.java
@@ -1,1 +1,28 @@
+public class House {
+  public Attr(String name) {
+    this.name = name;
+  }
 
+  public Attr(String name, String place, String description) {
+    this.name = name;
+    this.place = place;
+    this.desciption = description;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public String getPlace() {
+    return place;
+  }
+  
+  public String getDescription() {
+    return description
+  }
+  
+  public String toString() {
+    return name + "'" + place + "'" + description "";
+  }
+}  
+}
